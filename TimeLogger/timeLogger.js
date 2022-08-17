65a07941841218434d7cc485b514723c8dd143ec
SaiDrive <mail2saishankar@gmail.com>
2022-08-17 20:10:26 +0530

    created basic html, css, and javascript for TimeLogger

---
+++ TimeLogger/timeLogger.js
@@ -0,0 +1,20 @@
+
+
+let option;
+function learnIt(){
+    option = 1;
+    console.log(option)
+}
+
+function practiceIt(){
+    option = 2;
+    console.log(option)
+}
+
+
+function startTimer(){
+    if (option == 1){
+        option = option + 1;
+        console.log(option)
+    }
+}
