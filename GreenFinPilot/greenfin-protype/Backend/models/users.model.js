5f2d3dc52440c26f32ef0f0137212cd3d179df20
SaiDrive <mail2saishankar@gmail.com>
2022-11-08 15:02:40 +0530

    Added React, Express, Mongoose

---
+++ GreenFinPilot/greenfin-protype/Backend/models/users.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    userName: {
+        type : String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlenght: 3
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+        trim: true,
+        minlenght: 10
+    }
+
+},
+{ timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
